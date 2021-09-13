484b6634e1387a20fb31238824c46b5463e5f3c0
abdelrahman zaytoun <abdelrahmanzaytounn@gmail.com>
2021-09-12 22:22:04 -0400

    updating

---
+++ tweeter-master/server/composer-char-counter.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $('#tweet-text').on('input', function(event) {
+        if (this.value.lenght > 5) { alert("you exceeded the limits")}
+
+        console.log(this.value.lenght)}
+        )})
